10aae8fbb146b017183f2bf5b4fbd5e05a224ae2
Jora Troosh <tensorush@gmail.com>
2025-11-04 19:44:16 +0300

    fix: pub card init

--- src/Card.zig
+++ src/Card.zig
@@ -22,7 +22,7 @@ const FACTOR = std.math.pow(f64, 0.9, 1.0 / DECAY) - 1.0;
 const INIT_INTERVAL: zdt.Duration = .fromTimespanMultiple(10, .minute);
 const PARAMETERS = [21]f64{ 0.212, 1.2931, 2.3065, 8.2956, 6.4133, 0.8334, 3.0194, 0.001, 1.8722, 0.1666, 0.796, 1.4835, 0.0614, 0.2629, 1.6483, 0.6014, 1.8729, 0.5425, 0.0912, 0.0658, 0.1542 };
 
-const init: Card = .{
+pub const init: Card = .{
     .state = .Learning,
     .stability = undefined,
     .difficulty = undefined,
